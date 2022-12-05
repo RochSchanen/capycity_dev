f67f72e82b420f1eb95088a434867aae2060f398
Roch Schanen <r.schanen@lancaster.ac.uk>
2022-12-05 15:12:42 +0000

    improve config comments

--- capycity/config.py
+++ capycity/config.py
@@ -14,12 +14,12 @@ easily be checked empirically. As it turns out, a size of 32 bits seems
 to be the minimum required to provide a reliable solution. (This is
 higher than I personaly expected. I thought uint16 would be largely
 sufficient but it is not...). For development purposes, using 8 bits
-is a good ideas when checking operations on arrays. just uncomment the
+can be useful when checking operations on arrays. just uncomment the
 line next to use 8 bits range data type """
 # from numpy import uint8 as DATATYPE
 
 # define ZER and MAX values
 ZER, MAX = array([0, -1], DATATYPE)
-""" ZER is always represented by a chain of "zero" bits.
-MAX is always represented by a chain of "one" bits.
-The size of the chain depends on the integer size. """
+""" ZER is a chain of zeroes. MAX is a chain of ones. The size of the
+chain depends on the datatype. The two values ZER and MAX are defined
+in a way that insures the appropriate datatype. """
