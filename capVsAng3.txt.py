2a3d6d1b6a69099571595e929fdad39b512e5c8b
Roch Schanen <r.schanen@lancaster.ac.uk>
2023-05-25 21:41:28 +0100

    add analysis 3

--- capVsAng3.txt.py
+++ capVsAng3.txt.py
@@ -66,3 +66,158 @@ deg	deg	pF	pF
 37	43.5	28.19	15.0748663101604
 """
 
+# get list of line from text
+A, C = [], []
+for l in text.split(f"\n")[5:]:
+	V = l.split("\t")
+	if len(V) == 4:
+		A.append(float(V[0]))
+		C.append(float(V[2]))
+
+# convert lists into numpy arrays
+from numpy import array
+A, C = array(A), array(C)
+
+####################
+### USEFUL TOOLS ###
+####################
+
+def selectfigure(name):
+    from matplotlib.pyplot import figure, fignum_exists
+    if not fignum_exists(name):
+        # create figure
+        fg = figure(name)
+        # set A4 paper dimensions
+        fg.set_size_inches(8.2677, 11.6929)
+        # create square axis
+        w, h = array([1, 1 / 1.4143])*0.7
+        x, y = (1-w)/2, (1-h)/2
+        ax = fg.add_axes([x, y, w, h])
+    else:
+        # select figure
+        # (here the figure can be of any type)
+        fg = figure(name)
+        # get axes
+        ax = fg.get_axes()[0]
+    # done
+    return fg, ax
+
+class Document():
+
+    def __init__(self, pathname = None):
+        if pathname is not None:
+            self._DOC = self.opendocument(pathname)
+        return
+
+    def opendocument(self, pathname):
+        from matplotlib.backends.backend_pdf import PdfPages
+        self._DOC = PdfPages(pathname)
+        return self._DOC
+
+    def exportfigure(self, name):
+        args = selectfigure(name)
+        self._DOC.savefig(args[0])
+        return
+
+    def closedocument(self):
+        self._DOC.close()
+        return
+
+#########################
+### IDEAL CAPACITANCE ###
+#########################
+
+# the following function compute the overlap angle
+# from the angle of rotation (the origin is defined
+# when the overlap is maximum)
+def overlap(a):
+    # a: angle in degrees (origin at maximum)
+    
+    from numpy import mod, fabs
+    # mod(x,1.0) returns the fractional part of x
+    # fabs(x) returns the absolute value of x
+
+    # define boundaries, M: maximum overlap, T: period
+    M, T = 12.0, 30.0
+    
+    # compute overlap from angle
+    O = M-T*abs(mod(a/T+0.5, 1.0)-0.5)
+    
+    # nullify negative values
+    O[O<0] = 0
+    
+    # done
+    return O 
+
+def capacitance(angle, gap):
+    # angle: angle in degrees
+    # gap: gap in metres
+
+    from scipy.constants import epsilon_0 as E0
+    from numpy import pi, square, fabs
+
+    # overlap "eta" in radiants
+    eta = overlap(angle)*pi/180.0
+
+    # define star shape capacitance parameters
+    R1, R2 = 40E-3/2, 15E-3/2
+    N, L, l = 12, R1-R2, (R1+R2)/2.0
+
+    # compute star shape capacitance
+    C0 = N*E0*L*l/gap*eta
+
+    # define ring capacitance parameters
+    R3 = 13E-3/2
+
+    # compute ring capacitance
+    C1 = E0*(pi*square(R2)-pi*square(R3))/gap
+
+    # done
+    return (C0+C1)*1E12 # [pF]
+
+########################
+### PDF PLOT RESULTS ###
+########################
+
+# 40µm plastic sheet parameters:
+# e: measured dielectric constant relative to vacuum (epsilon)
+# g: measured thickness (capacitance gap)
+e, g = 1.818, 210E-6
+
+fg, ax = selectfigure("capa-vs-angle")
+
+# plot C versus A (shifted by 6.5 degree to set the origin)
+ax.plot(A, C/e, "k.-")
+
+# same plot shifed by "nearly" one period (30.5 degerees)
+# ax.plot(A-6.5 + 30.5, C/e, "k.-")
+
+# analytical plot
+ax.plot(A, capacitance(A, g), "r-")
+# ax.plot(A + 30.5, capacitance(A, g), "r-")
+
+# add labels
+ax.set_xlabel("angle [degrees]")
+ax.set_ylabel("Capacitance [pF]")
+
+# add vertical and horizontal guide lines
+ax.vlines(  [list(array([-12, -6, 0, +6, +12]))],
+            -10, 110, "b", "dashed", linewidth = 0.50)
+ax.vlines(  [list(array([-15, +15]))],
+            -10, 110, "r", "dashed", linewidth = 0.50)
+
+# modify ticks
+# from numpy import linspace
+# ax.set_xticks(linspace(-24, 48, 1+(48+24)//12))
+# ax.set_xticks(linspace(-24, 48, 1+(48+24)//6), minor = True)
+# ax.set_yticks(linspace(-20, 120, 1+(120+20)//10))
+# ax.set_yticks(linspace(-20, 120, 1+(120+20)//5), minor = True)
+
+# show grid on both axes
+# ax.grid("True", axis = "y", which = "both")
+ax.grid("True", axis = "both", which = "both")
+
+# export figure to pdf document
+doc = Document("../result.pdf") # file path + name
+doc.exportfigure("capa-vs-angle") # figure name
+doc.closedocument()
